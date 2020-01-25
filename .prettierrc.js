38a665cee6ba6282f8793f642a7ee0012444bc90
Jason Russell <jasrusable@gmail.com>
2020-01-25 10:50:17 +0200

    Tidy up

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "trailingComma": "all",
-  "tabWidth": 2,
-  "semi": true,
-  "singleQuote": true,
-  "printWidth": 80
-}
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+};
