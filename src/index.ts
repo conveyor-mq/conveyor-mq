644722bbb237c592a93bb1fc1ba11b9332af773f
Jason Russell <jasrusable@gmail.com>
2020-05-08 18:25:21 +0200

    Rename things

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export { acknowledgeTask } from './actions/acknowledge-task';
 export { areTasksStalled } from './actions/are-tasks-stalled';
 // eslint-disable-next-line import/no-cycle
-export { createWorker as createQueueWorker } from './actions/create-worker';
-export { createManager as createQueueManager } from './actions/create-manager';
+export { createWorker } from './actions/create-worker';
+export { createManager } from './actions/create-manager';
 // eslint-disable-next-line import/no-cycle
-export { createOrchestrator as createQueueOrchestrator } from './actions/create-orchestrator';
+export { createOrchestrator } from './actions/create-orchestrator';
 export { getProcessingTasks } from './actions/get-processing-tasks';
 export { getStalledTasks } from './actions/get-stalled-tasks';
 export { getTask } from './actions/get-task';
@@ -18,8 +18,8 @@ export { markTaskSuccess } from './actions/mark-task-success';
 export { markTasksFailed } from './actions/mark-tasks-failed';
 // eslint-disable-next-line import/no-cycle
 export { processStalledTasks } from './actions/process-stalled-tasks';
-export { enqueueStalledTasks as putStalledTasks } from './actions/enqueue-stalled-tasks';
-export { enqueueTask as putTask } from './actions/enqueue-task';
+export { enqueueStalledTasks } from './actions/enqueue-stalled-tasks';
+export { enqueueTask } from './actions/enqueue-task';
 export { enqueueTasks } from './actions/enqueue-tasks';
 export { takeTaskBlocking } from './actions/take-task-blocking';
 export { takeTask } from './actions/take-task';
