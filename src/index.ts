b5e16b0bbdcd37d103e52140aa911586cee68873
Jason Russell <jasrusable@gmail.com>
2020-05-30 14:51:49 +0200

    Export interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+// Functions
 export { createManager } from './actions/create-manager';
 export { createWorker } from './actions/create-worker';
 export { createOrchestrator } from './actions/create-orchestrator';
 export { createListener } from './actions/create-listener';
+
+// Utils
+export { loadLuaScripts } from './lua/index';
+
+// Enums
 export { EventType } from './domain/events/event-type';
 export { TaskStatus } from './domain/tasks/task-status';
+
+// Interfaces & types
+export { Task } from './domain/tasks/task';
+export { Event } from './domain/events/event';
+export { Worker } from './domain/workers/worker';
