aef82d6c835f0d7574a67d35dd55db5770df8e7f
Jason Russell <jasrusable@gmail.com>
2020-06-17 16:42:19 +0200

    Add cluster example

---
+++ examples/redis-cluster-example/index.ts
@@ -0,0 +1,20 @@
+import { createManager, createWorker } from 'conveyor-mq';
+
+const queueName = '{key}my-queue';
+const redisConfig = { host: '127.0.0.1', port: 6380, password: 'bitnami' };
+
+// Create a manager.
+const manager = createManager({ queue: queueName, redisConfig });
+
+// Enqueue a task.
+manager.enqueueTask({ data: { x: 1, y: 2 } });
+
+// Create a worker to process tasks.
+const worker = createWorker({
+  queue: queueName,
+  redisConfig,
+  handler: ({ task }) => {
+    console.log(`Processing task: ${task.id}`);
+    return task.data.x + task.data.y;
+  },
+});
